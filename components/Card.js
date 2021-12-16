42f1245c25bedbdcf738d0b89338ed5c22be92bd
90dandan <1brokendrumhead@gmail.com>
2021-12-16 13:12:05 -0600

    broken image replacement

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { TouchableOpacity, StyleSheet, Image, Text } from 'react-native';
+
+const placeholderImage = require('../assets/images/paper.png');
 
 class Card extends React.PureComponent {
     render() {
@@ -9,8 +11,13 @@ class Card extends React.PureComponent {
                 <Image 
                     resizeMode='cover'
                     style={styles.image} 
-                    source={{uri: 'https://image.tmdb.org/t/p/w500' + item.poster_path}}
+                    source={
+                        item.poster_path ?
+                        {uri: 'https://image.tmdb.org/t/p/w500' + item.poster_path}
+                        : placeholderImage
+                    }
                 />
+                {!item.poster_path && <Text style={styles.movieName}>{item.title}</Text>}
             </TouchableOpacity>
         );
     }
@@ -19,12 +26,21 @@ const styles = StyleSheet.create({
     container: {
         padding: 5,
         position: 'relative',
+        alignItems: 'center',
+        height: 200,
     },
     image: {
         height: 200,
         width: 120,
         borderRadius: 20,
     },
+    movieName: {
+        position: 'absolute',
+        width: 100,
+        top: 10,
+        textAlign: 'center',
+        
+    }
 })
 
 export default Card;
