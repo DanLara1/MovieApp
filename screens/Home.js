5a8d59ecc955c9ff1b7bd39440a86be061cdb3cd
90dandan <1brokendrumhead@gmail.com>
2021-12-17 14:19:52 -0600

    added loading spinner to home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState} from 'react';
-import { Text, View, StyleSheet, Dimensions, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, Dimensions, ScrollView, ActivityIndicator } from 'react-native';
 
 import { getPopularMovies, getUpcomingMovies, getPopularTv, getFamilyMovies, getDocumentary } from '../services/services';
 import { SliderBox } from 'react-native-image-slider-box'
@@ -9,13 +9,14 @@ import List from '../components/List';
 const dimentions = Dimensions.get('screen');
 const Home = () => {
     console.log(dimentions);
-    const [upcomingMovies, setUpcomingMovies] = useState('');
-    const [movieImages, setMovieImages] = useState('');
-    const [popularMovies, setpopularMovies] = useState('');
-    const [popularTv, setPopularTv] = useState('');
-    const [familyMovies, setFamilyMovies] = useState('');
-    const [documentary, setDocumentary] = useState('');
+    const [movieImages, setMovieImages] = useState();
+    const [popularMovies, setpopularMovies] = useState();
+    const [popularTv, setPopularTv] = useState();
+    const [familyMovies, setFamilyMovies] = useState();
+    const [documentary, setDocumentary] = useState();
+
     const [error, setError] = useState(false);
+    const [loaded, setLoaded] = useState(false);
     
     const getData = () => {
         return Promise.all([
@@ -29,45 +30,57 @@ const Home = () => {
 
   useEffect(() => {
 
-    getData().then(([upcomingMovies, popularMovies, popularTv, familyMovies, documentary]) => {
+    getData().then(([upcomingMoviesData, popularMoviesData, popularTvData, familyMoviesData, documentaryData]) => {
         const moviesImagesArray = [];
-        upcomingMovies.forEach(movie => {
+        upcomingMoviesData.forEach(movie => {
             moviesImagesArray.push('https://image.tmdb.org/t/p/w500' + movie.poster_path);
         });
         setMovieImages(moviesImagesArray);
-        setpopularMovies(popularMovies);
-        setPopularTv(popularTv);
-        setFamilyMovies(familyMovies);
-        setDocumentary(documentary);
+        setpopularMovies(popularMoviesData);
+        setPopularTv(popularTvData);
+        setFamilyMovies(familyMoviesData);
+        setDocumentary(documentaryData);
+        setLoaded(true);
     }).catch(err => [
         setError(err)
     ]);
   }, []);
     return (
     <react.Fragment>
-    <ScrollView> 
-    <View styles={styles.sliderContainer}>
-        <SliderBox 
-            images = {movieImages} 
-            dotStyle = {styles.sliderStyle}
-            sliderBoxHeight = {dimentions.height / 1} 
-            autoplay = {true} 
-            circleLoop = {true}
-        />
-    </View>
-    <View style={styles.carousel}>
-        <List title="Popular Movies" content={popularMovies} />
-    </View>
-    <View style={styles.carousel}>
-        <List title="Popular TV Shows" content={popularTv} />
-    </View>
-    <View style={styles.carousel}>
-        <List title="Family Movies" content={familyMovies} />
-    </View>
-    <View style={styles.carousel}>
-        <List title="Documentaries" content={documentary} />
-    </View>
-    </ScrollView>
+    {loaded && (
+        <ScrollView> 
+        {movieImages && (<View styles={styles.sliderContainer}>
+            <SliderBox 
+                images = {movieImages} 
+                dotStyle = {styles.sliderStyle}
+                sliderBoxHeight = {dimentions.height / 1} 
+                autoplay = {true} 
+                circleLoop = {true}
+            />
+        </View>) }
+        {popularMovies && (
+            <View style={styles.carousel}>
+                <List title="Popular Movies" content={popularMovies} />
+            </View>
+        )}
+        {popularTv && (
+            <View style={styles.carousel}>
+                <List title="Popular TV Shows" content={popularTv} />
+            </View>
+        )}
+        {familyMovies && (
+            <View style={styles.carousel}>
+                <List title="Family Movies" content={familyMovies} />
+            </View>
+        )}
+        {documentary && (
+            <View style={styles.carousel}>
+                <List title="Documentaries" content={documentary} />
+            </View>
+        )}
+        </ScrollView>
+    )}
+    {!loaded && <ActivityIndicator size="large"/>}
     </react.Fragment> 
     );
 }
