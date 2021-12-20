72d15dcc576dfdd7687106809602c1647a389270
90dandan <1brokendrumhead@gmail.com>
2021-12-20 13:24:37 -0600

    added modal for video when play button is pressed

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Image, ScrollView, Text, StyleSheet, Dimensions, ActivityIndicator, View } from 'react-native';
+import { Image, ScrollView, Text, StyleSheet, Dimensions, ActivityIndicator, View, Modal, Pressable } from 'react-native';
 import StarRating from 'react-native-star-rating';
 import { isDisabled } from 'react-native/Libraries/LogBox/Data/LogBoxData';
 import dateFormat from 'dateformat';
@@ -14,6 +14,7 @@ const Detail = ({route, navigation }) => {
 
     const [movieDetail, setMovieDetail] = useState();
     const [loaded, setLoaded] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
     const movieId = route.params.movieDetail.id;
 
     useEffect(() => {
@@ -23,9 +24,16 @@ const Detail = ({route, navigation }) => {
         });
     }, [movieId]);
 
+    {/* to swtich the modal oposite of current state, if false > true if true > false which will either
+    open the modal or close it */}
+    const showVideo = () => {
+        setModalVisible(!modalVisible);
+    };
+
     return (
         <React.Fragment>
             {loaded && (
+                <View>
                 <ScrollView >
                     <Image 
                     resizeMode='cover'
@@ -38,7 +46,7 @@ const Detail = ({route, navigation }) => {
                     />
                     <View style={styles.container}>
                         <View style={styles.playButton}>
-                            <PlayButton></PlayButton>
+                            <PlayButton handlePress={showVideo} />
                         </View>
                         <Text style={styles.movieTitle}>{movieDetail.title}</Text>
                         {movieDetail.genres && (
@@ -61,6 +69,14 @@ const Detail = ({route, navigation }) => {
                     <Text style={styles.overview}>{movieDetail.overview}</Text>
                     <Text style={styles.release}>{'Release Date: ' + dateFormat(movieDetail.release_date, 'dddd, mmmm ds, yyyy')}</Text>
                 </ScrollView>
+                <Modal animationType="slide" visible={modalVisible}>
+                    <View style={styles.videoModal}>
+                        <Pressable onPress={() => showVideo()}>
+                            <Text>{'Hide Modal'}</Text>
+                        </Pressable>
+                    </View>
+                </Modal>
+                </View>
             )}
             {!loaded && <ActivityIndicator size='large' />}
         </React.Fragment>
@@ -102,6 +118,11 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: -25,
         right: 20,
+    },
+    videoModal: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
 
